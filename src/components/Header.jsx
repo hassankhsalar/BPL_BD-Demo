1dbf285954b2ad948888b785bf428397412c2970
hassankhsalar <khsalarhassan@gmail.com>
2024-10-26 23:52:39 +0600

    finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({ coinCount }) => {
     return (
         <div className="navbar bg-base-100 w-11/12 mx-auto">
             <div className="flex-1">
-                <img className="w-16" src="./public/logo.JPG" alt="Logo" />
+                <img className="w-16" src="https://i.ibb.co.com/xHtCytz/logo-footer.png" alt="Logo" />
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
@@ -14,7 +14,7 @@ const Header = ({ coinCount }) => {
                     <li><a>Schedules</a></li>
                     <div className="flex items-center pt-2 ml-4">
                         <p className="mr-1">Coin: {coinCount.toLocaleString()}</p>
-                        <img className="w-4 h-4 mt-1 ml-1" src="./public/coin.png" alt="Coin Icon" />
+                        <img className="w-4 h-4 mt-1 ml-1" src="https://i.ibb.co.com/C0zX4SY/coin.png" alt="Coin Icon" />
                     </div>
                 </ul>
             </div>
